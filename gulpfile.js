d8fe75ef2b00fad4fafeaf4a3f287d11edf916d1
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-11-03 01:00:55 -0400

    update css

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,46 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+var gulp = require('gulp');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 
-  console.log("Hello World");
+  console.log("This is default action");
 
 });
 
-gulp.task('javis', function(){
+gulp.task('html', function(){
 
-  console.log("JAVIS: At your service sir, your file has been saved.");
+  console.log("I made some changes to html");
 
 });
+
+
+gulp.task('css', function(){
+
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles/'));
+
+});
+
+//start wotching...
 
 gulp.task('watch', function(){
 
   watch('./app/index.html', function(){
 
-    gulp.start('javis');
-    
-  })
+      gulp.start('html');
+
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+
+    gulp.start('css');
+
+  });
 
 });
